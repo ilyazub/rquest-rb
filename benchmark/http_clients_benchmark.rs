1137d6a79489f063d837af64054154523095626a
ilyazub <282605+ilyazub@users.noreply.github.com>
2025-03-20 13:35:53 +0100

    Simplify Rust HTTP client benchmarks

--- benchmark/http_clients_benchmark.rs
+++ benchmark/http_clients_benchmark.rs
@@ -1,32 +1,18 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
-use futures::future::join_all;
 use std::error::Error;
+use std::time::Duration;
 
-const REQUEST_COUNT: usize = 10;
 const URL: &str = "https://serpapi.com/robots.txt";
 
 fn reqwest_blocking() -> Result<(), Box<dyn Error>> {
     let client = reqwest::blocking::Client::new();
-    
-    for _ in 0..REQUEST_COUNT {
-        let _resp = client.get(URL).send()?;
-    }
-    
+    let _resp = client.get(URL).send()?;
     Ok(())
 }
 
 async fn reqwest_async() -> Result<(), Box<dyn Error>> {
     let client = reqwest::Client::new();
-    
-    let requests = (0..REQUEST_COUNT).map(|_| {
-        let client = client.clone();
-        async move {
-            client.get(URL).send().await?;
-            Ok::<(), Box<dyn Error>>(())
-        }
-    });
-    
-    join_all(requests).await;
+    let _resp = client.get(URL).send().await?;
     Ok(())
 }
 
@@ -34,22 +20,15 @@ async fn rquest_async() -> Result<(), Box<dyn Error>> {
     let client = rquest::Client::builder()
         .emulation(rquest_util::Emulation::Chrome134)
         .build()?;
-
-    let requests = (0..REQUEST_COUNT).map(|_| {
-        let client = client.clone();
-        async move {
-            client.get(URL).send().await?;
-            Ok::<(), Box<dyn Error>>(())
-        }
-    });
-    
-    join_all(requests).await;
+    let _resp = client.get(URL).send().await?;
     Ok(())
 }
 
 fn bench_http_clients(c: &mut Criterion) {
-    let mut group = c.benchmark_group("HTTP Clients - 10 GET requests");
-    
+    let mut group = c.benchmark_group("HTTP Client Comparison");
+
+    group.measurement_time(Duration::from_secs(10));
+
     group.bench_function(BenchmarkId::new("reqwest", "blocking"), |b| {
         b.iter(|| {
             reqwest_blocking().unwrap();
@@ -57,11 +36,8 @@ fn bench_http_clients(c: &mut Criterion) {
     });
     
     group.bench_function(BenchmarkId::new("reqwest", "async"), |b| {
-        b.iter(|| {
-            let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async {
-                reqwest_async().await.unwrap();
-            });
+        b.iter(async || {
+            reqwest_async().await.unwrap();
         });
     });
     
